a4f5a7bc3fe4433575a09d02ca1d702a0a334523
Chaitanya Pawar <chaitanya9pawar@gmail.com>
2025-05-19 19:59:37 +0530

    database

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,9 @@
-
 const express = require('express')
 const {getUsers, addUser, updateUser} = require('../controllers/userController')
 const router = express.Router()
 
 router.get("/users", getUsers)
 router.post("/users", addUser)
-router.put("/users/:email", updateUser)
+router.put("/users/:_id", updateUser)
 
 module.exports = router
